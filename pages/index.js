98dca71763fe396caa16fb9bee43cd06ff42a6e8
Rodrigo Mafra <rodrigo.a.mafra@gmail.com>
2026-07-14 02:18:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 function Home() {
-    return <h1>Viviane, meu amor, eu amo você! Se você me ama dá uma risadinha!!! =)<br/>
-    Que você aproveite seu dia de hoje!</h1>
+  return (
+    <h1>
+      Viviane, meu amor, eu amo você! Se você me ama dá uma risadinha!!! =)
+      <br />
+      Que você aproveite seu dia de hoje!
+    </h1>
+  );
 }
 
 export default Home;
